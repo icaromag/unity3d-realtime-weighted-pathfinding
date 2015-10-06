c1159fbff018200b6188bf8b2ef5155dc9bf32fe
Ícaro Magalhães <icaromagalhaes@lavid.ufpb.br>
2015-10-06 18:46:05 -0300

    created the grid of nodes required to begin using the A* search algorithm

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Node
+{
+    public bool walkable;
+    public Vector3 worldPosition;
+
+    public Node(bool _walkable, Vector3 _worldPosition)
+    {
+        walkable = _walkable;
+        worldPosition = _worldPosition;
+    }
+
+}
